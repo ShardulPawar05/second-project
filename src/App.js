fe22fa390301e9801026c109a12644fff98f32b8
shardul.pawar <shardulpawar53@gamil.com>
2025-08-19 16:49:17 +0530

    array program

--- src/App.js
+++ src/App.js
@@ -2,42 +2,26 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  let num1=10,num2=20;
-  let c=0;
+  let arr=[12,25,12,8,51,4,531,]
+  let ans;
+
+   arr.map((singleElemet)=>{
+     ans=2*singleElemet
+   })
   
-    function handleaddition(){
-     c=num1+num2;
-    console.log(c);
- 
-}
- function handlesub(){
-     c=num1-num2;
-    console.log(c);
- }
-  function handlemul(){
-     c=num1*num2;
-    console.log(c);
-  }
-   function handlediv(){
-     c=num1%num2;
-    console.log(c);
-   }
+
   
-    console.log(num1,num2)
-   
 
  
   return (
     <div>
-      <div>{num1}</div>
-      <div>{num2}</div>
-      
-
-      <button onClick={handleaddition}> addition</button><br/>
-       <button onClick={handlesub}> substarction</button><br/>
-        <button onClick={handlemul}> multiplication</button><br/>
-         <button onClick={handlediv}>division</button>
-         
+      <div> Your arry is<br/> {arr.map((singleelemet)=>{
+      return singleelemet+","
+    })}</div>
+     
+      <div> multiply arry by 2{arr.map((singleElemet)=>{
+      return 2*singleElemet+","
+   })}</div>
     </div>
   );
 }
