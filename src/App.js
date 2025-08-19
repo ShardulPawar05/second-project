f76c45f75895edb291b9eac1729d48cb1b1ac23b
shardul.pawar <shardulpawar53@gamil.com>
2025-08-19 16:21:04 +0530

    calulator program

--- src/App.js
+++ src/App.js
@@ -2,22 +2,42 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  let num1=10,num2=20;
+  let c=0;
+  
+    function handleaddition(){
+     c=num1+num2;
+    console.log(c);
+ 
+}
+ function handlesub(){
+     c=num1-num2;
+    console.log(c);
+ }
+  function handlemul(){
+     c=num1*num2;
+    console.log(c);
+  }
+   function handlediv(){
+     c=num1%num2;
+    console.log(c);
+   }
+  
+    console.log(num1,num2)
+   
+
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>{num1}</div>
+      <div>{num2}</div>
+      
+
+      <button onClick={handleaddition}> addition</button><br/>
+       <button onClick={handlesub}> substarction</button><br/>
+        <button onClick={handlemul}> multiplication</button><br/>
+         <button onClick={handlediv}>division</button>
+         
     </div>
   );
 }
