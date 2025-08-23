dc4c1d4707239fca606a40a835471def2abc7f09
shardul.pawar <shardulpawar53@gamil.com>
2025-08-23 12:24:13 +0530

    aceept two number from user and show addn substravtion maultiplication on scrren one by one

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import './App.css';
 function App() {
 const [num1,setnum1]=useState();
 const [num2,setnum2]=useState();
-parseInt(num1)
+
 // these variable use to store value of calculation
 const[totaladd,settotaladd]=useState();
 const[totalsub,settotalsub]=useState();
@@ -23,13 +23,35 @@ function handlenum1(event){
 function handlenum2(event){
   setnum2(parseInt(event.target.value))
 }
-function onSubmit(){
+function onADD(){
 
   settotaladd((num1)+(num2));
+  settotaldiv(undefined);
+  settotalmul(undefined);
+  settotalsub(undefined)
+}
+function onclickSub(){
+  
   settotalsub((num1)-(num2));
+    settotaladd(undefined);
+  settotaldiv(undefined);
+  settotalmul(undefined);
+  // settotalsub("")
+}
+function onClickMul(){
   settotalmul((num1)*(num2));
+    settotaladd(undefined);
+  settotaldiv(undefined);
+  // settotalmul("");
+  settotalsub(undefined)
+}
+function onClickDiv(){
   settotaldiv((num1)/(num2));
-
+    settotaladd(undefined);
+  // settotaldiv("");
+  settotalmul(undefined);
+  settotalsub(undefined);
+  
   
 
   // setshowblock(true)
@@ -44,12 +66,18 @@ function onSubmit(){
       <input type='number' name='num1' value={num1} onChange={handlenum1} placeholder='Enter the first number'></input> 
       <input type='number' name='num2' value={num2} onChange={handlenum2}    placeholder='Enter the second number'></input> 
       
-      <button onClick={onSubmit}>calulation</button>
-      {totaladd && <h1>addition of {num1} and {num2} is  { totaladd}</h1>}
-      {totalsub && <h1> substraction of {num1} and {num2} is {totalsub}</h1>}
-      {totalmul && <h1> multiplication of {num1} and {num2} is {totalmul}</h1>}
-      {totaldiv && <h1> divdation of {num1} and {num2} is {totaldiv}</h1>}
+      <br/> <button onClick={onADD}>Addition</button>
+      
+      <button onClick={onclickSub}>substraction</button>
+      
+      <button onClick={onClickMul }  >multiplication</button>
+     
+      <button onClick={onClickDiv}>Divadtion</button>
     
+    {totaladd !==undefined && <h1>addition of {num1} and {num2} is  { totaladd}</h1>}
+    {totalsub !==undefined &&<h1> substraction of {num1} and {num2} is {totalsub}</h1>}
+     {totalmul !==undefined&& <h1> multiplication of {num1} and {num2} is {totalmul}</h1>}
+     {totaldiv !==undefined && <h1> divdation of {num1} and {num2} is {totaldiv}</h1>}
 
       
       
